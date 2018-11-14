d24ac4250d1cf9ca1f31e4ed66531dbfe65e0f9d
Jesse D <jesse.davidson@umconnect.umt.edu>
2018-11-14 12:03:23 -0700

    Fixed solution to back_around

--- Warmup-1/back_around.py
+++ Warmup-1/back_around.py
@@ -3,5 +3,5 @@ so "cat" yields "tcatt". The original string will be length 1 or more.'''
 
 def back_around(str):
     if len(str) > 0:
-        str = str[len(str) - 1] + str + str[len(str) - 1]
+        str = str[-1] + str + str[-1]
     return str
