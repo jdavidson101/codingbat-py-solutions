63d2939286a28c930e70fd63daaeabf9b1c21d16
Jesse D <jesse.davidson@umconnect.umt.edu>
2018-11-14 11:59:26 -0700

    Added solutions and tests for front22

---
+++ Warmup-1/front22.py
@@ -0,0 +1,9 @@
+'''Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, 
+so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.'''
+
+def front22(str):
+    if len(str) <= 2:
+        str = str + str + str
+    else:
+        str = str[:2] + str + str[:2]
+    return str
